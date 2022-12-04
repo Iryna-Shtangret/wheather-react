8fefc2132b1e94f62a09e293bacf5105a388942e
Iryna-Shtangret <113630738+Iryna-Shtangret@users.noreply.github.com>
2022-12-05 01:12:10 +0200

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather React App</h1>
+
       <Weather />
+      <br />
+      <strong>
+        <a
+          href="https://github.com/Iryna-Shtangret/wheather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Iryna Mrachkovska
+      </strong>
     </div>
   );
 }
